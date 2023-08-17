e7d9e0a57ff7aedb503884d7583848e118603cec
Azis Zuhri Pratomo <aziszuhrip354@gmail.com>
2023-08-18 00:36:45 +0700

    update

---
+++ assets/tw-config.js
@@ -0,0 +1,11 @@
+const tailwind = require('https://cdn.tailwindcss.com');
+tailwind.config = {
+    fontFamiliy: "poppins",
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+}
